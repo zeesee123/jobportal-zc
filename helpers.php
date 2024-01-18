74182c8dcf40caee745ba55c284b6031ee5ce821
zeesee123 <zubinchadha@gmail.com>
2024-01-18 23:19:35 +0530

    Initial commit and started home view

---
+++ helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+
+function basepath($path=''){
+
+    return __DIR__.'/'.$path;
+
+}
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// require dirname(__DIR__).'\views\home.view.php';
+// require '..\views\home.view.php';
+
+require '../helpers.php';
+
+require basepath('views/home.view.php');
