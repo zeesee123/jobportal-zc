3bd1e94c098526d561d008f69d18622392eaff80
zeesee123 <zubinchadha@gmail.com>
2024-01-21 16:13:29 +0530

    working with htaccess

--- public/index.php
+++ public/index.php
@@ -8,4 +8,22 @@ require '../helpers.php';
 // require basepath('\views\home.view.php');
 // loadView("home");
 
-print_r($_SERVER['REQUEST_URI']);
+// print_r($_SERVER['REQUEST_URI']);
+$routes=[
+'/'=>'controllers\home.php',
+'/listings'=>'controllers\listings\index.php',
+'/listings/create'=>'controllers\listings\create.php',
+'404'=>'controllers\errors\404.php'
+];
+
+
+$uri=$_SERVER['REQUEST_URI'];
+
+if(array_key_exists($uri,$routes)){
+
+    require basepath($routes[$uri]);
+
+}else{
+
+    echo 'fuckyou';
+}
